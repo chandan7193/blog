120fb18edcac5ac40bcb264ccb775d163a70cd9e
chandan7193 <chandan.singh7193@gmail.com>
2017-11-04 17:05:11 +0530

    Small fixes

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,6 +1,7 @@
 import React,{Component} from 'react';
 import { connect } from 'react-redux';
 import { fetchPost } from '../actions';
+import {Link} from 'react-router-dom';
 
 
 class PostsShow extends Component{
@@ -10,10 +11,17 @@ class PostsShow extends Component{
     this.props.fetchPost(id);
   }
   render(){
+    const { post } =this.props;
+    if(!post){
+      return <div>Loading...</div>
+    }
 
     return(
       <div >
-        Post Show!!
+      <Link to="/"> Back to index</Link>
+        <h3>{post.title}</h3>
+        <h6>Categories:{post.categories}</h6>
+        <p>{post.content}</p>
       </div>
     );
   }
